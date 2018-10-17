41a7700af5b300b1411221575624e1b4f6cdeff1
Shreeganesh <sgbbhat@gmail.com>
2018-10-17 11:04:19 -0500

    adding models

--- tkinter/filedialog.py
+++ tkinter/filedialog.py
@@ -2,18 +2,16 @@
 from tkinter import filedialog
 from tkinter import *
 
-def readFile(name):
-	
-
 def showDialogButton():
 	global folder_path
-	filename = filedialog.askopenfilename(initialdir = "/", title = "Select files", filetypes = (("jpeg files","*.jpeg") , ("all files", "*.*")))
-	fileContent = readFile(filename)
-	folder_path.set(fileContent)
+	filename = filedialog.askopenfilename(initialdir = "/", title = "Select files", filetypes = (("text files", "*.txt") , ("all files", "*.*")))
+	folder_path.set(filename)
 
 root = Tk()
 folder_path = StringVar()
+button_pressed = BooleanVar()
 button2 = Button(text = "Browse", command = showDialogButton)
 button2.grid(row=0, column=3)
-print(getglobal())
+print(folder_path)
+
 root.mainloop()
