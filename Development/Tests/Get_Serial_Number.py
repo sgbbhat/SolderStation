17571d6918ae871fd36769be7169c0f7770cbbca
Shreeganesh <sgbbhat@gmail.com>
2018-11-28 17:24:24 -0600

    Process Enforcement Modification

--- Development/Tests/Get_Serial_Number.py
+++ Development/Tests/Get_Serial_Number.py
@@ -1,8 +1,8 @@
 # Function runs the stored procedure "getSerialNumber" taking Manufacturing Serial Number as input
 
 def getSerialNumber(databaseHandle, mfgID, MessageDisplaySlNo):
-	databaseHandle.execute("{CALL [dbo].[getSerialNumber] (?)}", mfgID)	
-	serialNumber = int(((databaseHandle.fetchall())[0])[0])
+	databaseHandle.execute("{CALL [dbo].[getSerialNumber] (?)}", mfgID)
+	serialNumber = ((databaseHandle.fetchall())[0])[0]
 	MessageDisplaySlNo.config(text = str(serialNumber), anchor = 'w')
 	databaseHandle.commit()
 	return serialNumber
--- Development/Tests/Process_Enforcement.py
+++ Development/Tests/Process_Enforcement.py
@@ -4,13 +4,21 @@ import re
 from Tests.displayResult import displayResult
 
 def Process_Enforcement(root, key, val, databaseHandle, mfgID, Sln, TestNameText, MinLimitText, MaxLimitText, MeasurementText, ResultText, modelFileContent, testStartTime, OperationMode, OperationModeInput, LotNumvberInput):
-	databaseHandle.execute("Select distinct ProcessFlowKey from dbo.TestEvent WHERE MfgSerialNumber = ? AND ProcessFlowKey != 0", mfgID)
-	ProcessFlowKey = databaseHandle.fetchall()
-	CurrentPartNumber = modelFileContent['Part_No']
-	CurrentProcessStep = 'Board Test'
-	databaseHandle.execute("Select PrePartNumber from dbo.ProcessEnforcementStep WHERE ProcessFlowKey = ? AND CurrentPartNumber = ? AND CurrentProcessStep = ?", int((ProcessFlowKey[0])[0]), CurrentPartNumber[0], CurrentProcessStep)
-	PrePartNumber = databaseHandle.fetchall()
-	print(PrePartNumber)	
+	# Running a stored procedure - getFlowKey
+	getFlowKeyParam = (Sln, (modelFileContent['Part_No'])[0])
+	databaseHandle.execute("{CALL [dbo].[getFlowKey] (?, ?)}", getFlowKeyParam)
+	ProcessFlowKey = ((databaseHandle.fetchall())[0])[0]
+	databaseHandle.commit()
+
+	# Running a stored procedure - getProcessStep
+	CurrentPartNumber = (modelFileContent['Part_No'])[0]
+	CurrentProcessStep = (modelFileContent['Current_Step'])[0]
+	getProcessStepParam = (ProcessFlowKey, CurrentProcessStep, CurrentPartNumber)
+	databaseHandle.execute("{CALL [dbo].[getProcessStep] (?, ?, ?)}", getProcessStepParam)
+	getProcessStepReturn = databaseHandle.fetchall()
+	ProcessStep = (getProcessStepReturn[0])[0]
+	PrePartNumber = (getProcessStepReturn[0])[1]
+
 	if bool(PrePartNumber) == False:
 		PrePartNumberTestResult_lookup = [()]
 		PrePartNumberTestResult = [()]
@@ -19,7 +27,6 @@ def Process_Enforcement(root, key, val, databaseHandle, mfgID, Sln, TestNameText
 		databaseHandle.execute("Select Passed from dbo.InProcess WHERE  MfgSerialNumber = ? AND PartNumber = ? " , mfgID, 'ERG7100005-Curie')
 		PrePartNumberTestResult = databaseHandle.fetchall()
 
-	print(PrePartNumberTestResult)
 	PrePartNumberTestResult_lookup = [item for item in PrePartNumberTestResult if 1 in item]
 	if bool(PrePartNumberTestResult_lookup) == False:
 		result = "Fail"
