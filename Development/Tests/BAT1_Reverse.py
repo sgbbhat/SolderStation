61383e76c09c3d5ec1f4d66414de8f2f97bfbc48
Shreeganesh <sgbbhat@gmail.com>
2018-11-30 19:20:12 +0000

    Testing

--- Development/Tests/BAT1_Reverse.py
+++ Development/Tests/BAT1_Reverse.py
@@ -6,6 +6,8 @@ import re
 from Tests.displayResult import displayResult
 from tkinter import messagebox 
 
+
+
 def BAT1_Reverse(root, key, val, databaseHandle, mfgID, Sln, TestNameText, MinLimitText, MaxLimitText, MeasurementText, ResultText, modelFileContent, testStartTime, OperationMode, OperationModeInput, LotNumvberInput):
 	measurement = popen('megaio 0 optread 8').read()
 	mod_TestName = re.sub(r"(\w)([A-Z])", r"\1 \2", key)
